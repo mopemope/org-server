e2d9fa5bb30280e5e4ccd2c747d21ae1926b7d13
mopemope <yutaka.matsubara@gmail.com>
2024-01-08 09:51:49 +0900

    feat: add server base

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,1 +1,28 @@
+use anyhow::Result;
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 
+#[tokio::main]
+async fn main() -> Result<()> {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
