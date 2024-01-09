e97f9314d89bf65906edea99abd3ae3c5765777d
mopemope <yutaka.matsubara@gmail.com>
2024-01-09 15:22:44 +0900

    feat: add init tracing

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,12 +5,27 @@ use axum::{
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
+    init_tracing();
+    run_server().await?;
+    Ok(())
+}
 
+fn init_tracing() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "org_server=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+}
+
+async fn run_server() -> Result<()> {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
@@ -18,6 +33,7 @@ async fn main() -> Result<()> {
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    info!("start server");
     axum::serve(listener, app).await?;
     Ok(())
 }
