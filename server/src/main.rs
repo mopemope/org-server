a498bb416212720d43e14281da5398acbe399320
mopemope <yutaka.matsubara@gmail.com>
2024-01-12 22:12:22 +0900

    feat: add cli command

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,55 @@
-use anyhow::Result;
+use anyhow::Context as _;
+use anyhow::{ensure, Result};
 use axum::{
     http::StatusCode,
     routing::{get, post},
     Json, Router,
 };
+use clap::Parser;
 use serde::{Deserialize, Serialize};
-use tracing::info;
+use std::path::PathBuf;
+use tracing::{debug, info};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+pub const APP_NAME: &str = "org-server";
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct App {
+    #[arg(short, long)]
+    config: Option<String>,
+
+    #[arg(short, long)]
+    port: Option<u32>,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     init_tracing();
-    run_server().await?;
+
+    let app = App::parse();
+    let config_path = if let Some(path) = app.config.as_deref() {
+        PathBuf::from(path)
+    } else {
+        get_config_file("org-server.toml")?
+    };
+
+    let port = if let Some(port) = app.port {
+        port
+    } else {
+        3000
+    };
+
+    /*
+    ensure!(
+        config_path.exists(),
+        "fail load config. {:?} not exists",
+        config_path
+    );
+     */
+
+    debug!("load config path: {:?}", config_path);
+    run_server(port).await?;
     Ok(())
 }
 
@@ -25,14 +63,20 @@ fn init_tracing() {
         .init();
 }
 
-async fn run_server() -> Result<()> {
+fn get_config_file(name: &str) -> Result<PathBuf> {
+    let xdg_dir =
+        xdg::BaseDirectories::with_prefix(APP_NAME).context("failed get xdg directory")?;
+    xdg_dir.place_config_file(name).context("failed get path")
+}
+
+async fn run_server(port: u32) -> Result<()> {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
     info!("start server");
     axum::serve(listener, app).await?;
     Ok(())
