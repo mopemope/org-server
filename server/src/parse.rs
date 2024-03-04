5769d89884bd087a247a832f3449a7f90a0d438f
mopemope <yutaka.matsubara@gmail.com>
2024-03-04 16:35:45 +0900

    feat: add get_remainders

--- server/src/parse.rs
+++ server/src/parse.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
+use org_parser::{Org, Remainder};
 use std::path::Path;
 use tokio::io::AsyncReadExt;
 use tokio::{fs::File, task};
-use tracing::debug;
 use walkdir::WalkDir;
 
 async fn parse_org_file(path: &Path) -> Result<org_parser::Org> {
@@ -16,19 +16,36 @@ async fn parse_org_file(path: &Path) -> Result<org_parser::Org> {
     Ok(org)
 }
 
-pub async fn parse_org_files(path: &str) -> Result<()> {
+pub async fn parse_org_files(path: &str) -> Result<Vec<Org>> {
     let mut handles = vec![];
 
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
         let path = entry.path().to_owned();
         if let Some(ext) = path.extension() {
             if ext == "org" {
-                let handle = task::spawn(async move {
-                    // let now = time::Instant::now();
+                let handle = task::spawn(async move { parse_org_file(&path).await });
+                handles.push(handle);
+            }
+        }
+    }
 
-                    // debug!("{} {:?}", path.display(), now.elapsed());
-                    parse_org_file(&path).await
-                });
+    let all_res = futures::future::join_all(handles).await;
+
+    let mut orgs = vec![];
+    for org in all_res.into_iter().flatten().flatten() {
+        orgs.push(org);
+    }
+    Ok(orgs)
+}
+
+pub async fn get_remainders(path: &str) -> Result<Vec<Remainder>> {
+    let mut handles = vec![];
+
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+        let path = entry.path().to_owned();
+        if let Some(ext) = path.extension() {
+            if ext == "org" {
+                let handle = task::spawn(async move { parse_org_file(&path).await });
 
                 handles.push(handle);
             }
@@ -37,11 +54,11 @@ pub async fn parse_org_files(path: &str) -> Result<()> {
 
     let all_res = futures::future::join_all(handles).await;
 
-    for res in all_res {
-        if let Ok(org) = res {
-            // TODO parse reminder
-            debug!("{:?}", org);
-        }
+    let mut res = vec![];
+
+    for org in all_res.into_iter().flatten().flatten() {
+        let mut remainders = org.get_remainders();
+        res.append(&mut remainders);
     }
-    Ok(())
+    Ok(res)
 }
